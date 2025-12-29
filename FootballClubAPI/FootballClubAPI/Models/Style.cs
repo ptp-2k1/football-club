372d30b2a661a8b82e7325e2312990116ff36ce3
Phung Tin Phong <phongphungkile@gmail.com>
2025-12-29 23:24:03 +0700

    removed c# API from windows branch

--- FootballClubAPI/FootballClubAPI/Models/Style.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FootballClubAPI.Models
-{
-    public class Style
-    {
-        public int id {  get; set; }
-        public int clubId {  get; set; }
-        public string Name { get; set; } = null!;
-    }
-}
